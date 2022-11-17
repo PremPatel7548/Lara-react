d1d22b59604a671319ad0058f37f8906db1905fa
KartikmPatel <kartikmpatel0804@gmail.com>
2022-11-17 15:00:09 +0530

    hash

--- app/Http/Controllers/usercontroller.php
+++ app/Http/Controllers/usercontroller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use App\Models\UserModel;
 
 class usercontroller extends Controller
@@ -12,7 +13,7 @@ class usercontroller extends Controller
         $user=new UserModel;
         $user->name=$req['name'];
         $user->email=$req['email'];
-        $user->password=$req['password'];
+        $user->password=Hash::make($req['password']);
         $user->save();
     }
 }
